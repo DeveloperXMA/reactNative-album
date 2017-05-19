749cc80ce11d98fcb324cdf71c951d50c486dd3a
Xinrui Ma <xinrui.ma@teamaol.com>
2017-05-19 14:56:18 -0400

    add header style

--- src/components/header.js
+++ src/components/header.js
@@ -2,19 +2,29 @@ import React, { Component } from 'react';
 import {
   Text,
   StyleSheet,
+  View
  } from 'react-native';
 
 export default class Header extends Component {
   render() {
     return (
-      <Text style={styles.textStyle}>
-        Albums!
-      </Text>
+      <View style={styles.viewStyle}>
+        <Text style={styles.textStyle}>
+          Albums!
+        </Text>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15
+  },
   textStyle: {
     fontSize: 20
   }
