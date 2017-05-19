3a0e61f97fb32f356be61f824703f20c0ab36760
Xinrui Ma <xinrui.ma@teamaol.com>
2017-05-19 14:39:07 -0400

    adding Header

--- index.ios.js
+++ index.ios.js
@@ -8,24 +8,15 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
-  Text,
-  View
+  View,
 } from 'react-native';
+import Header from './src/components/header';
 
 export default class todayRoutine extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <View>
+        <Header></Header>
       </View>
     );
   }
